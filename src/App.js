ce4952117e081c4eb0c3da4fcf24437bea2728b0
HSGill <harjotgill73@gmail.com>
2020-10-10 18:06:44 +1300

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import Menu from './components/menu component/menu.component.jsx';
 import Shop from './pages/shop/shop.component.jsx';
 import Home from'./pages/Home/home.component.jsx';
 import Contact from  './pages/contact/contact.component.jsx';
+import {  Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Menu />
-      <Home />
-      <Shop />
-      <Contact />
+      <Switch>
+        <Route path='/' component= {Home} exact />
+        <Route path='/shop' component= {Shop}/>
+
+        <Route path='/contact' component= {Contact}/>
+
+      </Switch>
+
+      
 
 
      </div>
--- src/components/menu component/menu.component.jsx
+++ src/components/menu component/menu.component.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import './menu.styles..scss';
+import { Link } from 'react-router-dom';
+import LOGO from '../../assets/LOGO.png'
+import { ReactComponent as Logo } from '../../assets/LOGO.png'
 
-class Menu extends React.Component{
-    constructor (props){
-        super(props);
-        this.state={
-            route :''
-        }
-
-    }
-    
-    render(){
-        return (
-            <div className='menu-container'>
-            <div className='logo'>Logo</div>
-            <div>Shop</div>
-            <div>Contact</div>
-            </div>
-        )
-    }
+const Menu = () =>{
+    return (
+        <div className='menu-container'>
+        <Link className="logo-container" to="/"><img src={LOGO} alt="logo"></img></Link>
+        <div className="options" >
+        <Link className="option" to="/shop">SHOP</Link>
+        <Link className="option" to="/contact">CONTACT</Link>
+        </div>
+        </div>
+    )
 }
- 
 
 export default Menu;
